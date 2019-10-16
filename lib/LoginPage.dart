dad04b3a28f8c216ab08e854f8a1c35858c49a0c
Pham Tuan <phamtuan199911@gmail.com>
2019-10-16 21:35:14 +0700

    Re-add numeric keyboard Add "I don't have phone number" Inkwell Add contact info Change Blend color

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -27,7 +27,7 @@ class _LoginPageState extends State<LoginPage> {
                 image: AssetImage("assets/background.png"),
                 fit: BoxFit.fill,
                 colorFilter: ColorFilter.mode(
-                    Colors.black.withOpacity(0.2), BlendMode.darken)),
+                    Colors.blueGrey.withOpacity(0.6), BlendMode.darken)),
           ),
           child: BackdropFilter(
             filter: ImageFilter.blur(
@@ -57,9 +57,9 @@ class _LoginPageState extends State<LoginPage> {
                                 fontStyle: FontStyle.normal,
                                 fontSize: 38.0),
                           ),
-                        ),
+                        ), // "Welcome to Zenspa" Text
                         Padding(
-                          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                          padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
                           child: Text(
                             "Enter your phone number to check in",
                             style: TextStyle(
@@ -69,10 +69,10 @@ class _LoginPageState extends State<LoginPage> {
                                 fontStyle: FontStyle.normal,
                                 fontSize: 17.0),
                           ),
-                        ),
+                        ), // "Enter your phone number to check in" Text
                         Container(
                           padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-                          width: 400,
+                          width: 307,
                           height: 100,
                           child: TextField(
                             decoration: InputDecoration(
@@ -82,19 +82,62 @@ class _LoginPageState extends State<LoginPage> {
                                 ),
                                 focusedBorder: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(10.0),
-                                    borderSide: BorderSide(color: Colors.white)
-                                ),
+                                    borderSide:
+                                    BorderSide(color: Colors.white)),
                                 hintText: "111-222-3333",
                                 hintStyle: _InputTextStyle(),
                                 contentPadding:
-                                EdgeInsets.fromLTRB(125, 20, 0, 5)),
+                                EdgeInsets.fromLTRB(75, 20, 0, 5)),
                             style: _InputTextStyle(),
                             keyboardType: TextInputType.phone,
                             cursorColor: Colors.white,
                           ),
-                        ),
-                        NumericKeyboard()
+                        ), // Textfield
+                        NumericKeyboard(),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: InkWell(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  "I don't have phone number",
+                                  style: TextStyle(
+                                      color: const Color(0xe6ffffff),
+                                      fontWeight: FontWeight.w400,
+                                      fontFamily: "SFProDisplay",
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 17.0),
+                                ),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: CupertinoColors.white,
+                                )
+                              ],
+                            ),
+                            onTap: () {
+
+                            },
+                          ),
+                        ), // "I don't have phone number" Inkwell
                       ],
+                    ),
+                  ),
+                  Positioned(
+                    child: Align(
+                      alignment: FractionalOffset.bottomCenter,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                        child: Text(
+                          "Power by Zentech \u2022 By sale contact us: 6308546553",
+                          style: TextStyle(
+                              color: CupertinoColors.white,
+                              fontWeight: FontWeight.w300,
+                              fontFamily: "SFProDisplay",
+                              fontStyle: FontStyle.normal,
+                              fontSize: 13.0),
+                        ),
+                      ),
                     ),
                   )
                 ],
@@ -107,13 +150,16 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-class _InputTextStyle extends TextStyle{
+class _InputTextStyle extends TextStyle {
   @override
   Color get color => Colors.white;
+
   @override
   FontWeight get fontWeight => FontWeight.w400;
+
   @override
   String get fontFamily => "SFProDisplay";
+
   @override
   double get fontSize => 25.0;
 }
