2383f84cdda6a0de1de44417e8672fb7ddce599c
Pham Tuan <phamtuan199911@gmail.com>
2019-10-17 07:00:09 +0700

    Re-add numeric keyboard Add "I don't have phone number" Inkwell Add contact info Change Blend color Remove button shadow

--- lib/NumericKeyboard.dart
+++ lib/NumericKeyboard.dart
@@ -5,29 +5,73 @@ class NumericKeyboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 230,
-      width: 400,
-      child: GridView.count(
-          crossAxisCount: 3,
-          childAspectRatio: 2.9,
-          mainAxisSpacing: 12,
-          physics: NeverScrollableScrollPhysics(),
-          padding: EdgeInsets.all(2.0),
-          children: List.generate(12, (index) {
-            return Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-              child: FloatingActionButton(
-                child: Text("$index"),
-                onPressed: () {},
-                backgroundColor: Colors.grey.withOpacity(0.5),
-                foregroundColor: Colors.white,
-              ),
-            );
-          })),
+      height: 250,
+      width: 307,
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                _getButton(1),
+                _getButton(2),
+                _getButton(3)
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                _getButton(4),
+                _getButton(5),
+                _getButton(6)
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                _getButton(7),
+                _getButton(8),
+                _getButton(9)
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                _getButton(10),
+                _getButton(0),
+                _getButton(11)
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
+}
+
+Widget _getButton(int buttonType) {
+  Widget buttonChild;
+  if (buttonType < 10) {
+    buttonChild = Text("$buttonType");
+  } else {
+    if (buttonType == 10) {
+      buttonChild = Icon(Icons.backspace);
+    } else if (buttonType == 11) {
+      buttonChild = Icon(Icons.done);
+    } else {
+      buttonChild = Text("illegal parameter");
+    }
+  }
+
+  return Expanded(
+    child: FloatingActionButton(
+      child: buttonChild ?? Text("Null"),
+      onPressed: () {},
+      foregroundColor: CupertinoColors.white,
+      backgroundColor: CupertinoColors.white.withOpacity(0.3),
+      elevation: 0.0,
+    ),
+  );
 }
